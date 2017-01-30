4ad283c7639da654f47ce604af696a4ab49c3883
Nicholas Kennedy <nick.kennedy@rangle.io>
2017-01-29 19:24:19 -0500

    Folder structure and readme

--- Lesson-1/3-Fetch/2-Post/post.js
+++ Lesson-1/3-Fetch/2-Post/post.js
@@ -7,7 +7,9 @@ const jsonPlaceholderApiUrl = 'https://jsonplaceholder.typicode.com/posts';
 // https://developers.google.com/web/updates/2015/03/introduction-to-fetch#post_request
 
 
-//When you send this, the response should be the same object with a randomly assigned id for the new post
+// When you send this, the response should be the same object with a randomly assigned id for the new post
+// This is so if you want to request the post again, or update the data, or show it somewhere else your application
+// Now knows the ID
 const postToAdd = {
    "userId": 1,
    "title": "Test",
--- Lesson-1/JSON/Parsing/Readme.md
+++
@@ -1,4 +0,0 @@
-### Parsing JSON with JSON.parse
-
-Part 1 shows parsing data from a string and printing it out
-Part 2 shows how we can use the parsed data to display things in HTML
