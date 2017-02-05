470f5e31830e4f56e4a97f4300f8f9a42dc0c0c5
Nicholas Kennedy <nick.kennedy@rangle.io>
2017-02-05 16:05:01 -0500

    Works

--- Assignment1-Server/index.js
+++ Assignment1-Server/index.js
@@ -1,10 +1,33 @@
 const koa = require('koa');
-const cors = require('koa-cors');
-const router = require('koa-simple-router');
+const Router = require('koa-router');
 const fetch = require('node-fetch');
-const fs = require('fs');
+const cors = require('kcors');
 
 const bikeShareURL = 'https://feeds.bikesharetoronto.com/stations/stations.json';
 
+let app = new koa();
+let router = new Router();
+let response = {};
 
-fetch(bikeShareURL).then( response => response.json()).then(console.log);
+app.use(cors());
+
+const updateData = () => {
+  fetch(bikeShareURL)
+    .then( response => response.json())
+    .then(data => {
+    response = data;
+    }).catch( err => fs.appendFile('log.txt', err))
+  };
+
+// Update data every 6 hours
+updateData();
+setInterval(updateData, 1000 * 60 * 60 * 6);
+
+router.get('/', function(ctx, next) {
+  ctx.body = response;
+})
+app.use(router.routes())
+   .use(router.allowedMethods());
+
+app.listen(8081);
+console.log('Running');
