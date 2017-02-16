9b03b156a4d912cfc47d643ef8301cd52d52cbfa
Nicholas Kennedy <nick.kennedy@rangle.io>
2017-02-15 22:50:55 -0500

    FacebookSDK

--- Lesson-4/Server/index.js
+++ Lesson-4/Server/index.js
@@ -2,7 +2,9 @@ const koa = require('koa');
 const serve = require('koa-serve');
 
 const app = koa();
+app.use(serve('index.html'));
 app.use(serve('1-FacebookSDK/index.html'));
+app.use(serve('2-ReadPosts/index.html'));
 
 app.listen(3000);
 console.log('listening at http://localhost:3000');
