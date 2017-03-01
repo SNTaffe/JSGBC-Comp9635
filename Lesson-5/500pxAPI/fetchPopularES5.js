f7a5a041a3f1dcccecb5c5c8b68078c00ba4fac6
Nicholas Kennedy <nick.kennedy@rangle.io>
2017-02-28 21:44:11 -0500

    ES5

---
+++ Lesson-5/500pxAPI/fetchPopularES5.js
@@ -0,0 +1,16 @@
+// Using the API key and the documentation found here, query for popular photos
+// https://github.com/500px/api-documentation
+
+var consumerKey = 'SIwRLq0AwiEydJeCT8NLk3chVNyEMgweH52bQVx5';
+var feature = 'popular';
+
+var baseUrl = 'https://api.500px.com/v1/'
+var photoUrl = 'photos'
+//apiUrl/photos?consumer_key=key&feature='popular'
+fetch(baseUrl + photoUrl + 'consumer_key=' + consumerKey + '&feature=' + feature)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+  }
