63fcb451ba26f04a7e951fb7b5152ad46d89d99d
Graphic <graphic@SOD-SJH-517-32MT.local>
2017-03-02 21:14:15 -0500

    Solutions, fix

--- Lesson-6/SandboxExamples/getComments.js
+++ Lesson-6/SandboxExamples/getComments.js
@@ -1,1 +1,16 @@
 // Use the sandbox API to get all the comments for photoId 'inclass'.
+const apiKey = 'b2becbb33a01c2a1085888f14dda841901646b3d8543ac0a925275050cc82f81';
+
+const baseURL = 'http://138.197.158.83:8081/';
+
+const photoId = 'inclass';
+
+fetch(`${baseURL}/api/photos/${photoId}/comments?apiKey=${apiKey}`).then(function(response) {
+  if (response.ok) {
+    return response.json();
+  }
+  console.log('There was an error.', response.status);
+  // Should maybe also do response.json() here to display the error message
+}).then(function(data) {
+  console.log(data);
+});
--- Lesson-6/SandboxExamples/postComment.js
+++ Lesson-6/SandboxExamples/postComment.js
@@ -1,1 +1,36 @@
 // Using your API key, post a comment to the Sandbox for the photo ID 'inclass'
+
+// Your request will need these things or you will get a 400
+// Query Param = apiKey=<a valid API key>
+// Body = Object with text property
+// Method = POST
+// Header = Content-Type: 'application/json'
+
+const commentObj = {
+  text: 'Some text',
+};
+
+const options = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(commentObj),
+}
+
+const apiKey = 'b2becbb33a01c2a1085888f14dda841901646b3d8543ac0a925275050cc82f81';
+
+const baseURL = 'http://138.197.158.83:8081/';
+
+const photoId = 'inclass';
+
+
+fetch(`${baseURL}/api/photos/${photoId}/comments?apiKey=${apiKey}`, options)
+  .then(function(response) {
+    if (response.ok) {
+      return response.json();
+    }
+    console.log('There was an error', response.status);
+}).then(function(data) {
+  console.log(data);
+})
